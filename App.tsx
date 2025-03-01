a5a8aa9a17e8111f926959e8e31becd0506ced1c
Harsh Sangrulkar <harshsangrulkar@gmail.com>
2025-03-01 11:26:21 +0530

    styling goals list display

--- App.tsx
+++ App.tsx
@@ -1,17 +1,17 @@
 import React, { useState } from "react";
 import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
 
-const App = ()=>{
+const App = () => {
 
-  const [goal,setGoal]=useState('');
-  const [list,setList]=useState<string[]>([])
+  const [goal, setGoal] = useState('');
+  const [list, setList] = useState<string[]>([])
 
-  const goalInputHandler =(eneterText:string)=>{
+  const goalInputHandler = (eneterText: string) => {
     //console.log(eneterText);
     setGoal(eneterText);
   }
 
-  const addGoalHandler=()=>{
+  const addGoalHandler = () => {
     //display the entered value
     //for this first save the entered value in a state
     //console.log(goal);
@@ -19,63 +19,90 @@ const App = ()=>{
     //syntax
     /*
     setState((prevState)=>(prevState + newElement))
-    */ 
-    setList((currentGoals)=>[...currentGoals,goal]);
+    */
+    setList((currentGoals) => [...currentGoals, goal]);
     setGoal("");
   }
 
-  return(
+  return (
     <SafeAreaView style={styles.container}>
       <View style={styles.inputContainer}>
-      <TextInput style={styles.textinput} placeholder="Your course Goal" onChangeText={goalInputHandler}></TextInput>
-      <TouchableOpacity style={styles.button} onPress={addGoalHandler}><Text>Add Goal</Text></TouchableOpacity>
+        <TextInput style={styles.textinput} placeholder="Your course Goal"
+          value={goal}
+          onChangeText={goalInputHandler}></TextInput>
+        <TouchableOpacity style={styles.button} onPress={addGoalHandler}><Text style={styles.btnText}>Add Goal</Text></TouchableOpacity>
       </View>
-      <Text style={styles.text}>List of Goals</Text>
-      {/* list.map((value)=><Text>{value}<Text/>) */}
-      {list.map((g,index)=><Text key={index}>{g}</Text>)}
+
+      <View style={styles.goalContainer}>
+        <Text style={styles.text}>List of Goals</Text>
+        {/* list.map((value)=><Text>{value}<Text/>) */}
+        {list.map((value, index) => <View key={index}>
+          <Text style={styles.goalItem}>{value}</Text>
+        </View>)}
+      </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent:"center",
-    alignItems:"center",
-    borderColor:"black",
-    borderWidth:3,
-    borderRadius:10,
-    margin:15,
-    padding:5,
-    backgroundColor:"lightgray"
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    //alignItems: "center",
+    borderColor: "black",
+    borderWidth: 3,
+    borderRadius: 10,
+    margin: 15,
+    padding: 5,
+    backgroundColor: "lightgray"
   },
-  text:{
-    fontSize:24,
-    color:"black",
+  text: {
+    fontSize: 24,
+    color: "black",
+    fontWeight: "bold",
+    margin: 5,
   },
-  inputContainer:{
-    // flex:1,
-    flexDirection:"row",
-    justifyContent:"space-between",
-    alignItems:"center",
-    margin:10,
-    padding:25,
-    borderColor:"black",
-    borderWidth:2,
-    
+  inputContainer: {
+    //flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    margin: 10,
+    marginTop: 30,
+    padding: 25,
+    // borderColor: "black",
+    borderBottomColor: "black",
+    //borderWidth: 2,
+
   },
-  textinput:{
-    flex:1,
-    borderColor:"black",
-    borderWidth:2,
-    borderRadius:5,
-    padding:5,
-    margin:5,
+  textinput: {
+    flex: 1,
+    borderColor: "black",
+    borderWidth: 2,
+    borderRadius: 5,
+    padding: 5,
+    margin: 5,
   },
-  button:{
-    borderColor:"black",
-    padding:5,
-    backgroundColor:"blue",
+  button: {
+    borderColor: "black",
+    padding: 5,
+    backgroundColor: "blue",
+
+  },
+  btnText: {
+    color: "white",
+  },
+  goalContainer: {
+    flex: 5,
+  },
+  goalItem: {
+    margin: 5,
+    padding: 10,
+    backgroundColor: "purple",
+    borderRadius: 5,
+    color: "white",
+    fontSize: 16,
+
   }
 })
 
